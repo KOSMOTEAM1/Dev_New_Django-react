eb7462771002927849a997bd0575d57872da6c6f
POOHYA <hyoju2259@gmail.com>
2022-01-07 17:45:50 +0900

    효주 한국영화순으로 100개 정렬

--- backend/themovieDB/views.py
+++ backend/themovieDB/views.py
@@ -3,7 +3,7 @@ from django.views import generic
 from django.views.generic.dates import ArchiveIndexView
 from rest_framework import generics
 from themovieDB.models import movie
-from .serializers import PostSerializer, RecentSerializer
+from .serializers import PostSerializer, SortSerializer
 
 class ListPost(generics.ListCreateAPIView):
     queryset = movie.objects.all()
@@ -15,4 +15,8 @@ class DetailPost(generics.RetrieveUpdateDestroyAPIView):
 
 class RecentPost(generics.ListCreateAPIView):
     queryset = movie.objects.filter(status = 'Released').values('otteid','title').order_by('-release_date')[:100]
-    serializer_class = RecentSerializer
+    serializer_class = SortSerializer
+
+class KoreaPost(generics.ListCreateAPIView):
+    queryset = movie.objects.filter(original_language = 'ko').values('otteid','title').order_by('?')[:100]
+    serializer_class = SortSerializer
