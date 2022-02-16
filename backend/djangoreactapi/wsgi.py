ffafe2092df36a1e73637eacafc7dd2e0dcf57c0
POOHYA <hyoju2259@gmail.com>
2022-02-16 15:07:11 +0900

    효주 도커완성오예

--- backend/djangoreactapi/wsgi.py
+++ backend/djangoreactapi/wsgi.py
@@ -7,10 +7,25 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+""" import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoreactapi.settings')
 
-application = get_wsgi_application()
+application = get_wsgi_application() """
+
+
+
+import os
+import sys
+
+path = '/home/ejsong21/my-first-blog'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'djangoreactapi.settings'
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
