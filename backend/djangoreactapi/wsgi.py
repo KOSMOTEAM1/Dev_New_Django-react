c88626968565161a804353188fe7433d5ed215ab
POOHYA <hyoju2259@gmail.com>
2022-02-18 17:02:36 +0900

    효주 배포중

--- backend/djangoreactapi/wsgi.py
+++ backend/djangoreactapi/wsgi.py
@@ -7,14 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-""" import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoreactapi.settings')
-
-application = get_wsgi_application() """
-
 import os
 import sys
 
